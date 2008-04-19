31ec2bf822264279201633a30cb70a0f080e627c
Anders Pearson <anders@columbia.edu>
2008-04-19 13:43:05 -0400

    updated config for dev

--- urls.py
+++ urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^fusite/', include('fusite.foo.urls')),
-    (r'^$','fusite.fu.views.index'),
-    # Uncomment this for admin:
-     (r'^admin/', include('django.contrib.admin.urls')),
+                       (r'^$','fusite.fu.views.index'),
+                       # Uncomment this for admin:
+                       (r'^admin/', include('django.contrib.admin.urls')),
+                       # serve media in dev mode
+                       (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/home/anders/code/python/fusite/media/'}),
 )

2c4a49ff76866bb0eb8817fd91837e7ad1bdd165
Anders Pearson <anders@columbia.edu>
2008-04-20 14:30:56 -0400

    added basic article page

--- fu/views.py
+++ fu/views.py
@@ -16,3 +16,10 @@ def issue(request,year,month,day):
     main_article = i.main_article()
     return render_to_response("issue.html",dict(issue=i,
                                                 main_article=main_article))
+
+def article(request,year,month,day,slug):
+    i = Issue.objects.get(pub_date="%04d-%02d-%02d" % (int(year),int(month),int(day)))
+    a = list(i.article_set.filter(slug=slug))[0]
+    return render_to_response("article.html",
+                              dict(issue=i,
+                                   article=a))
--- urls.py
+++ urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
                        (r'^$','fusite.fu.views.index'),
                        (r'^issues/(?P<year>\d+)\-(?P<month>\d+)\-(?P<day>\d+)/$', 'fusite.fu.views.issue'),
+                       (r'^issues/(?P<year>\d+)\-(?P<month>\d+)\-(?P<day>\d+)/(?P<slug>[^/]+)/$', 'fusite.fu.views.article'),                       
                        (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/home/anders/code/python/fusite/media/'}),
                        (r'^admin/', include('django.contrib.admin.urls')),
 )

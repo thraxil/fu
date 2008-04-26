0a8b648da0f75f8b041d9327a1be66f6b3207434
Anders Pearson <anders@columbia.edu>
2008-04-26 17:09:33 -0400

    atom feeds added

--- urls.py
+++ urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls.defaults import *
+from fusite.fu.feeds import MainFeed
+
+feeds = dict(main=MainFeed)
 
 urlpatterns = patterns('',
                        (r'^$','fusite.fu.views.index'),
@@ -8,4 +11,5 @@ urlpatterns = patterns('',
                        (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/home/anders/code/python/fusite/media/'}),
                        (r'^uploads/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '/tmp/fusite/data'}),                       
                        (r'^admin/', include('django.contrib.admin.urls')),
+                       (r'^feeds/(?P<url>.*)/$','django.contrib.syndication.views.feed', {'feed_dict': feeds}),
 )
